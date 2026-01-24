c945edd03aa55ff3d231c2ee27edce7a41946c0c
devoid-of-thought <mikynowy@gmail.com>
2026-01-24 15:19:33 +0100

    Ujednolicenie wyglądu index.php i register.php

--- login/register.php
+++ login/register.php
@@ -13,6 +13,9 @@ if (empty($_SESSION['csrf_token'])) {
 <head>
     <meta charset="UTF-8">
     <title>Rejestracja - Planszówki</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
 </head>
 
@@ -21,15 +24,20 @@ if (empty($_SESSION['csrf_token'])) {
         <div class="main-content">
             <div class="form-container">
                 <h2>Zarejestruj się</h2>
-                <form action="../api/register.php" method="POST">
+                
+                <form action="../api/register.php" method="POST" style="width: 100%;">
                     <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
 
                     <input type="text" name="username" placeholder="Nazwa użytkownika" required>
                     <input type="email" name="email" placeholder="Adres e-mail" required>
                     <input type="password" name="password" placeholder="Hasło" required>
-                    <button class="btn-small save" type="submit">Załóż konto</button>
+                    <button class="btn-small" type="submit" style="width: 100%;">Załóż konto</button>
+
+                    <div style="display: flex; align-items: center; margin-bottom: 15px; gap: 10px;">
+                        <a href="index.php" style="margin-left: auto; font-size: 0.9em; text-decoration: none; color: var(--color-magenta);">Masz już konto? Zaloguj się</a>
+                    </div>
+
                 </form>
-                <a href="index.php" style="margin-left: auto; margin-top:1em; font-size: 1.2em; text-decoration: none; color: var(--color-magenta);">Masz już konto? Zaloguj się</a>
 
             </div>
         </div>
